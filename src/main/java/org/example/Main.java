197b1952ce38b4fb62c614448f62c8421e7d0522
JoshuaQuan <joshuaquan24@crescentschool.org>
2026-05-01 13:00:54 -0400

    hi jack, how are you doing?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int x = 5; 
+        int y = 6; 
+        System.out.println(x * y); 
     }
 }
